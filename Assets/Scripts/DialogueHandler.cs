5d1a640f81442cf7c6bdf5257479db7172ed26be
jaynie <jaynie.r.lea@gmail.com>
2023-10-21 19:06:40 -0700

    added character sprites

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueHandler : MonoBehaviour
 {
-    public Button goNext;
+    //public Button goNext;
+    //public TextMeshProUGUI text;
+    private Queue<string> dialogue;
 
+    private void Start()
+    {
+        dialogue = new Queue<string>();
+    }
 
 }
